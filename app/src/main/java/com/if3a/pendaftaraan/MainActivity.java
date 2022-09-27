ea2465688ceb27d930d3709eeb21da5c8005c17c
Viky <vikyhendriko03@mhs.mdp.ac.id>
2022-09-27 16:53:11 +0700

    Pendaftaraan

---
+++ app/src/main/java/com/if3a/pendaftaraan/MainActivity.java
@@ -0,0 +1,28 @@
+package com.if3a.pendaftaraan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnDaftar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnDaftar = findViewById(R.id.btn_daftar);
+        btnDaftar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(MainActivity.this, "Anda Sudah Terdaftar !", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
